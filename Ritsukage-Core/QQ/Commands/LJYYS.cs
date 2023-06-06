585f14b955691334333c98ee21bac62adbc71f6f
KunoSayo <KunoSayo@outlook.com>
2023-06-06 13:23:23 +0800

    that's it.

---
+++ Ritsukage-Core/QQ/Commands/LJYYS.cs
@@ -0,0 +1,21 @@
+﻿using Ritsukage.Tools;
+using Sora.Entities.Segment;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ritsukage.QQ.Commands
+{
+    [CommandGroup("LJYYS")]
+    public static class LJYYS
+    {
+        [Command("ljyys")]
+        [CommandDescription("ljyys")]
+        public static async void Generate(SoraMessage e)
+        {
+            await e.ReplyToOriginal(SoraSegment.Image("ljyys.jpg"));
+        }
+    }
+}
