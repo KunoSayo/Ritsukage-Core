f450fcca14ad88576b0a4f82035b5dc9a9039dc1
BAKAOLC <ritsukage@qq.com>
2023-06-06 11:50:41 +0800

    init

---
+++ src/Ritsukage-Core.Common/Logging/CoreLogger.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using NLog;
+using NLog.Config;
+using NLog.Extensions.Logging;
+
+namespace RUCore.Common.Logging
+{
+    /// <summary>
+    /// NLog logger
+    /// </summary>
+    public static class CoreLogger
+    {
+        static readonly ISetupBuilder Builder;
+        static CoreLogger()
+        {
+            var config = new ConfigurationBuilder().Build();
+            Builder = LogManager.Setup().SetupExtensions(ext => ext.RegisterConfigSettings(config));
+        }
+
+        /// <summary>
+        /// Gets the specified named logger.
+        /// </summary>
+        /// <param name="name">name of Logger</param>
+        /// <returns></returns>
+        public static Logger GetLogger(string name)
+            => Builder.GetLogger(name);
+    }
+}
---
+++ src/Ritsukage-Core/Main.cs
@@ -0,0 +1,41 @@
+﻿using NLog;
+using RUCore.Common.Logging;
+using System.Diagnostics;
+using System.Text;
+
+Console.Title = "Ritsukage Core";
+DateTime LaunchTime = DateTime.Now;
+Logger logger = CoreLogger.GetLogger("Main");
+
+AppDomain.CurrentDomain.UnhandledException += (sender, args) =>
+{
+    var now = DateTime.Now;
+    var sb = new StringBuilder()
+        .AppendLine("！！！程序发生未捕获异常，程序即将崩溃！！！")
+        .AppendLine($"启动于 {LaunchTime:yyyy-MM-dd HH:mm:ss.ffff}")
+        .AppendLine($"崩溃于 {now:yyyy-MM-dd HH:mm:ss.ffff}")
+        .Append($"工作时长 {now - LaunchTime}");
+    logger.Fatal(args.ExceptionObject as Exception, sb.ToString());
+};
+
+var currentProcess = Process.GetCurrentProcess();
+_ = new Mutex(true, currentProcess.ProcessName, out var isFirst);
+if (isFirst)
+{
+    logger.Info("程序启动");
+    await Task.CompletedTask; // TODO: repalce for program logic
+    logger.Info("程序结束");
+}
+else
+{
+    Console.WriteLine("请勿重复启动程序实例，以免发生异常");
+}
+if (!Console.IsInputRedirected)
+{
+    Console.WriteLine("程序主逻辑已结束，按任意键结束程序");
+    Console.ReadKey(true);
+}
+else
+{
+    Console.WriteLine("程序主逻辑已结束");
+}
